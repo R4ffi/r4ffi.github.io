f1cda15a66a7e6e947cbfef8635a3c04f4bc495d
Raffael Nussbaumer <raffael.nussbaumer@isolutions.ch>
2023-09-15 15:34:24 +0200

    Add background

--- script/main.js
+++
@@ -1,7 +0,0 @@
-function sidebar_open() {
-  document.getElementById("mySidebar").style.display = "block";
-}
-
-function sidebar_close() {
-  document.getElementById("mySidebar").style.display = "none";
-}
